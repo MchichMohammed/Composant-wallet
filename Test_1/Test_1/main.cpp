bba09bd45c85d57c519368f12eb5d45a289b3107
MchichMohammed <MchichMohammed@github.com>
2019-04-10 21:53:16 +0200

    after the merge with composant1

---
+++ Test_1/Test_1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "FileInterface.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "TEST COMPOSANT 1" << endl;
+	FileInterface f("..\\..\\Composant1\\livreDeCompte.json"); // attention au path du projet
+	f.verification();
+	Bloc response = f.findByIndex(2);
+	f.insert(response);
+	cout<<"Resultat toString : " <<f.toString(true,2)<<endl;
+
+
+	int blocker;
+	cin >> blocker;
+
+	return 0;
+}
---
+++ composant2/targetver.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Si vous incluez SDKDDKVer.h, cela définit la dernière plateforme Windows disponible.
+
+// Si vous souhaitez générer votre application pour une plateforme Windows précédente, incluez WinSDKVer.h et
+
+#include <SDKDDKVer.h>
